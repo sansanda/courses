1ee4040fcbdad28bf1a66076ceb29682ed08c02d
sansanda <dsanchezsanc@uoc.edu>
2018-12-07 13:25:44 +0100

    Node MongoDB Example 2

--- NodeJS/node-mongo/index.js
+++ NodeJS/node-mongo/index.js
@@ -1,12 +1,15 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require('./dbOperations');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
 
 function checkDatabaseExistence(_db,_dbname)
 {
+    //WARNING!!!! The database could exists but no collection could be inside it!!!
     _db.admin().listDatabases(function(err, result) {
+        console.log(result);
         var ret = JSON.stringify(result.databases).includes("\"name\":"+"\""+_dbname+"\"");
         assert.equal(ret,true,"Inexistent Database: name = "+_dbname+" !!!!");
         //throw new Error("Something went badly wrong!");
@@ -18,29 +21,31 @@ function dbConnectResult(err,client) {
     assert.equal(err,null,err);
     console.log('Connected correctly to server');
     const db = client.db(dbname);
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
+    "dishes", (result) => {
+        console.log("Insert Document:\n", result.ops);
+
+        dboper.findDocuments(db, "dishes", (docs) => {
+            console.log("Found Documents:\n", docs);
+
+            dboper.updateDocument(db, { name: "Vadonut" },
+                { description: "Updated Test" }, "dishes",
+                (result) => {
+                    console.log("Updated Document:\n", result.result);
+
+                    dboper.findDocuments(db, "dishes", (docs) => {
+                        console.log("Found Updated Documents:\n", docs);
+                        
+                        db.dropCollection("dishes", (result) => {
+                            console.log("Dropped Collection: ", result);
+
+                            client.close();
+                        });
+                    });
+                });
+        });
+    });
     
-    checkDatabaseExistence(db,dbname);
-
-
-    const collection = db.collection('dishes',(error,collection)=>{
-        assert.equal(err,null,'error fetching the collection');    
-    });
-
-
-    collection.insertOne({"name":"Uthapizza","description":"Test"},(err,result)=>{
-        assert.equal(err,null);
-        console.log('After insert\n');
-        console.log(result.ops);
-        collection.find({}).toArray((err,docs)=>{
-            assert.equal(err,null);
-            console.log('Found\n');
-            console.log(docs);
-            db.dropCollection('dishes',(err,result)=>{
-                assert.equal(err,null,'Error dropping dishes collection');
-                client.close();
-            });
-        });    
-    });
 }
 
 MongoClient.connect(url,{ connectTimeoutMS: 5000 },dbConnectResult);
