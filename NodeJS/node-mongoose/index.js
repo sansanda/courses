24cf90bd02168d4eedccba642ad95213cacd317f
sansanda <dsanchezsanc@uoc.edu>
2018-12-08 10:42:40 +0100

    Mongoose ODM Part 2

--- NodeJS/node-mongoose/index.js
+++ NodeJS/node-mongoose/index.js
@@ -8,34 +8,30 @@ dbConnection
 .then(
     (db)=>{
         console.log('Connected corretly to the server');
-        var newDish = dishesModel({name: 'Uthappizza',description: 'Test'});
-        newDish.save()
-        .then(
-            (dish)=>{
-                console.log("Added dish: \n");
-                console.log(dish);
-                return dishesModel.find({});
-            }    
-        )
-        .then(
-            (dishes)=>{
-                console.log("Dishes List: \n");
-                console.log(dishes);
-                return dishesModel.remove({});
-            }    
-        )
-        .then(
-            ()=>{
-                console.log("Removing all dishes... \n")
-                return;
-            }    
-        )
-        .then(
-            ()=>{
-                console.log("Closing Connection... \n")
-                return mongoose.connection.close();
-            }    
-        )
+        dishesModel.create({
+            name: 'Uthapizza',
+            description: 'Test'
+        })
+        .then((dish) => {
+            console.log("Added dish: \n");
+            console.log(dish);
+            
+            return dishesModel.find({}).exec();
+        })
+        .then((dishes) => {
+            console.log("Dishes List: \n");
+            console.log(dishes);
+    
+            return dishesModel.remove({});
+        })
+        .then(() => {
+            console.log("Removed all dishes. \n")
+            console.log("Closing Connection... \n")
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
     }    
 )
 .catch(
